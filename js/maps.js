e4e964e4f99849d9186b71f403dfaff11a42bf81
Aidan Stewart <na.stewart365@gmail.com>
2023-02-25 14:05:09 -0500

    Polling Locations

--- js/maps.js
+++ js/maps.js
@@ -4,6 +4,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
+/*
+Mapping
 L.Routing.control({
     waypoints: [
         L.latLng(57.74, 11.94),
@@ -11,3 +13,17 @@ L.Routing.control({
     ],
     routeWhileDragging: true
 }).addTo(map);
+*/
+
+/*
+GeoJSON Polling Locations
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'lib/geojson/Polling_Locations.geojson');
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.responseType = 'json';
+xhr.onload = function() {
+    if (xhr.status !== 200) return
+    L.geoJSON(xhr.response).addTo(map);
+};
+xhr.send();
+*/
