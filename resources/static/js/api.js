9fe750e37e90f96d7539a84b1970bfdc5e7b6278
Aidan Stewart <na.stewart365@gmail.com>
2023-02-26 09:14:36 -0500

    Rollback

--- resources/static/js/api.js
+++
@@ -1,6 +0,0 @@
-function email(){
-    fetch("/email?full-name=" + document.getElementById("full-name").value + "&email-address=" + document.getElementById("email-address").value,
-    {
-        method: "POST",
-    })
-}
