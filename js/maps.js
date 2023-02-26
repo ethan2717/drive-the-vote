eb97d92c55d896eea646d8e61aa2d6fbc69d487f
Aidan Stewart <na.stewart365@gmail.com>
2023-02-25 20:21:35 -0500

    Finished driving

--- js/maps.js
+++ js/maps.js
@@ -1,60 +1,59 @@
 var map = L.map('map').setView([42.35532753176672, -71.06532863539319], 13);
 
 const driver = {
-    origin: L.latLng(0,0),
-    destination: L.latLng(0,0),
-    available: true,
-    control: null,
-    marker: null,
-    simulate : function (){
-        
-        if (this.control != null){
-            map.removeControl(this.control);
-            this.control = null;
-        }
-        this.control = L.Routing.control({
-            waypoints: [this.origin, this.destination],
+    _control: null,
+    _marker: null,
+    _route: function (origin, destination){
+        this._control = L.Routing.control({
+            waypoints: [origin, destination],
             draggableWaypoints: false,
             addWaypoints: false,
-          
             createMarker: function(i, waypoint, n) {
                 return null;
             }
         })
-        this.control.addTo(map);
-        this.control.hide();
-
-        let polylinePromise = new Promise((resolve, reject) => {
-            this.control.on('routeselected', (e) => {
+        this._control.addTo(map);
+        this._control.hide();
+    },
+    _simulate : function (duration){
+        let simulatePromise = new Promise((resolve, reject) => {
+            this._control.on('routeselected', (e) => {     
                 let route = e.route;
                 let polylineCoords = route.coordinates;
-                let polyline = L.polyline(polylineCoords).addTo(map);
-                polyline.setStyle({
-                    color: 'blue'
-                });
+                let polyline = L.polyline(polylineCoords)
                 resolve(polyline);
+                map.removeControl(this.control);
             });
         });
-
-        polylinePromise.then((polyline) => {
-            var coordinateArray = polyline.getLatLngs();
-            var myMovingMarker = L.Marker.movingMarker(coordinateArray, 6000, {
-                autostart: false
+        simulatePromise.then((polyline) => {
+            var coordinateArray = polyline.getLatLngs(); 
+            if (this._marker != null)
+                map.removeLayer(this._marker);   
+            this._marker = L.Marker.movingMarker(coordinateArray, duration, {
+                autostart: true
             });
-            map.addLayer(myMovingMarker);
-            myMovingMarker.start();
-           
+            map.addLayer(this._marker);
+            this._marker.on('end', () => {
+                map.removeLayer(polyline);       
+                document.dispatchEvent(new CustomEvent('driverarrived'));     
+            });
         });
-        
-         
     },
-
-
+    drive: function(origin, destination, duration=10000) {
+        this._route(origin, destination)
+        this._simulate(duration)
+    }
 }
 
-const newDriver = {...driver, origin: L.latLng(42.35532753176672, -71.06532863539319), destination: L.latLng(42.33727616231241, -71.09772940232675)}
+const newDriver = {...driver}
 
-newDriver.simulate();
+
+newDriver.drive(L.latLng(42.35532753176672, -71.06532863539319), L.latLng(42.33727616231241, -71.09772940232675));
+document.addEventListener('driverarrived', () => {
+    newDriver.drive(L.latLng(42.33727616231241, -71.09772940232675), L.latLng(42.35532753176672, -71.06532863539319));
+
+});
+  
 
 
 async function addGeoJson() {
@@ -102,7 +101,7 @@ var driverIco = L.icon({
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
-L.marker([42.364875718764935, -71.05748723613675], {icon: driverIco}).addTo(map);
+L.marker([42.364875718764935, -71.05748723613675], {icon: driver}).addTo(map);
 
 
 /*
