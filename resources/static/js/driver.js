e1798d9753fe0191b9df83ec7b701c69a8fe88d4
Aidan Stewart <na.stewart365@gmail.com>
2023-02-26 06:40:04 -0500

    Gitignore and resources

---
+++ resources/static/js/driver.js
@@ -0,0 +1,84 @@
+var names = ['Sophia', 'Ethan', 'Ava', 'Jackson', 'Olivia', 'Aiden', 'Emma', 'Lucas', 'Mia', 'Liam'];
+var services = ["Uber", "Lyft", "Taxi"]
+var cars = ['SUV', 'Sedan', 'Coupe', 'Hatchback', 'Convertible', 'Pickup Truck', 'Sports Car', 'Minivan', 'Station Wagon', 'Electric Car'];
+var licensePlates = ['ABCD-123', 'GHIJ-456', 'MNOP-789', 'WXYZ-321', 'JKL-654', 'VWX-987', 'STU-246', 'DEF-135', 'PQR-680', 'LMN-925'];
+
+const driver = {
+    _origin: null,
+    _control: null,
+    _marker: null,
+    service: "",
+    name: "",
+    car: "",
+    licensePlate: "",
+
+    _route: function (origin, destination){
+        if (this._origin != null && this._origin.lat == origin.lat && this._origin.lng == origin.lng != null)
+            return;
+        this._origin = origin
+        this._control = L.Routing.control({
+            waypoints: [origin, destination],
+            draggableWaypoints: false,
+            addWaypoints: false,
+            createMarker: function(i, waypoint, n) {
+                return null;
+            }
+        })
+        this._control.addTo(map);
+        this._control.hide();
+    },
+
+    _simulate : function (duration){
+        let simulatePromise = new Promise((resolve, reject) => {
+            this._control.on('routeselected', (e) => {     
+                let route = e.route;
+                let polylineCoords = route.coordinates;
+                let polyline = L.polyline(polylineCoords)
+                resolve(polyline);
+                map.removeControl(this._control)
+            });
+        });
+        simulatePromise.then((polyline) => {
+            var coordinateArray = polyline.getLatLngs(); 
+            if (this._marker != null) {
+                map.removeLayer(this._marker);  
+            }
+            var driverIco = L.icon({
+                iconUrl: "./images/car.png",
+                iconSize: [18, 18], 
+                popupAnchor: [0, -10],
+              });
+            this._marker = L.Marker.movingMarker(coordinateArray, duration, {
+                autostart: true,
+                icon: driverIco,
+                autoCenter: false
+            }); 
+            this._marker.on('end', () => {    
+                if (this.called != true)
+                    map.removeLayer(this._marker)
+                document.dispatchEvent(new CustomEvent('driverarrived', {detail: {name: this.name}}));     
+            });
+            //Localize
+            if (this.name == "" && this.service == ""){
+                this.name = names[Math.floor(Math.random() * names.length)]
+                this.service = services[Math.floor(Math.random() * services.length)]
+                this.car = cars[Math.floor(Math.random() * cars.length)]
+                this.licensePlate = licensePlates[Math.floor(Math.random() * licensePlates.length)]
+            }
+            this._marker.bindPopup(`
+            Rideshare Service: `+ this.service + ` <br>
+            Name: `+ this.name + `<br>
+            Car: `+ this.car + `<br>
+            License Plate: `+ this.licensePlate + `<br>
+            
+            `).openPopup();
+            this._marker.addTo(map);
+        });
+    },
+
+    drive: function(origin, destination, duration=10000) {
+        this._route(origin, destination)
+        this._simulate(duration)
+    },
+}
+
---
+++ resources/static/js/map.js
@@ -0,0 +1,82 @@
+var map = L.map("map", {
+    center: [42.35532753176672, -71.06532863539319],
+    zoom: 13,
+}).setView([42.35532753176672, -71.06532863539319], 13);
+
+var drivers = [];
+var ballot = null;
+var currentLocation;
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors",
+}).addTo(map);
+
+
+fetch('/lib/geojson/Polling_Locations.geojson')
+.then((response) => response.json())
+.then((json) => {
+    var ballotIco = L.icon({
+        iconUrl: "./images/voting-box.png",
+        iconSize: [25, 25], // size of the icon
+        popupAnchor: [0, -10],
+      });
+    for (var i=0; i < json.features.length; i++) {
+        let feature = json.features[i];
+        if (feature.geometry != null) {
+            let coordinates = feature.geometry.coordinates;
+            let ballotMarker = L.marker([coordinates[1], coordinates[0]], {
+                icon: ballotIco
+            }).addTo(map);
+            ballotMarker.bindPopup(`
+            <p>`+ feature.properties.Location2 + `</p>
+            <p>`+ feature.properties.Location3 + `</p>
+            <p>`+ (feature.properties.Voting_Room || " ") + `</p>
+            `).openPopup();
+            ballotMarker.on('click', (e) => {
+                ballot = e.target;  
+                console.log(ballot)
+            });
+        }
+     }
+});
+
+map.locate({maxZoom: 16});
+
+map.on('locationfound', (e) => {
+    currentLocation = L.marker(e.latlng).addTo(map);
+    currentLocation.bindPopup("You are here!").openPopup();
+});
+
+
+function locateDrivers(){
+    var currentCenter = map.getCenter();
+    var currentZoom = map.getZoom();
+    for (var i = 0; i < 5; i++) {   
+      
+        let origin_lat = ballot.getLatLng().lat + (Math.random() - 0.5) / 50;
+        var origin_lng = ballot.getLatLng().lng + (Math.random() - 0.5) / 50;
+        var dest_lat = origin_lat + (Math.random() - 0.5) / 50;
+        var dest_lng = origin_lng + (Math.random() - 0.5) / 50;
+        volunteerDriver = {...driver}
+        volunteerDriver.drive(L.latLng(origin_lat, origin_lng), L.latLng(dest_lat, dest_lng), 15000);
+        drivers.splice(i, 0, volunteerDriver);
+    } 
+    setTimeout(() => {
+        map.setView(currentCenter, currentZoom);
+      }, 3000)
+   
+}
+
+function requestDriver(){
+    volunteerDriver = {...driver}
+    var origin_lat = ballot.getLatLng().lat + (Math.random() - 0.5) / 150;
+    var origin_lng = ballot.getLatLng().lng + (Math.random() - 0.5) / 150;
+    volunteerDriver.drive(L.latLng(origin_lat, origin_lng), L.latLng(currentLocation.getLatLng().lat, currentLocation.getLatLng().lng));
+    document.addEventListener('driverarrived', (e) => {
+        if (e.detail.name == volunteerDriver.name) {
+            volunteerDriver.drive(L.latLng(currentLocation.getLatLng().lat, currentLocation.getLatLng().lng), L.latLng(ballot.getLatLng().lat, ballot.getLatLng().lng), 16000);
+        }
+    });
+}
+
+
