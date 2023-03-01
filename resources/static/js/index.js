b2dbc07b3ed6c438a97d8eb448a2629fec236e24
Aidan Stewart <na.stewart365@gmail.com>
2023-02-28 23:12:12 -0500

    Update index.js

--- resources/static/js/index.js
+++ resources/static/js/index.js
@@ -41,3 +41,20 @@ $(document).ready(function() {
         }
     });
 });
+
+function email(){
+    fetch("/email?full-name=" + document.getElementById("full-name").value + "&email-address=" + document.getElementById("email-address").value,
+    {
+        method: "POST",
+    });
+}
+
+function chat() {
+    fetch("/chat?input=" + document.getElementById("chat").value + "?key=" + document.getElementById("chat"),
+    {
+        method: "POST",
+    }).then((response) => response.text())
+    .then((text) => {
+      document.getElementById("chat-responce").innerHTML = text;
+    });
+}
